368ad8ab1aae98356a68cda44072e6d14203f6e9
niaochaobao <niaochaobao@163.com>
2014-12-21 20:38:13 +0800

    add login

--- dunk/handler/user/login.py
+++ dunk/handler/user/login.py
@@ -1,11 +1,29 @@
 # coding: utf8
-
+import tornado
 from tornado.web import RequestHandler
 from dunk.utils.template import st
 from dunk.utils.decorators import render
 
+from dunk.handler.api.auth import check
+
 
 class LoginHandler(RequestHandler):
+    @tornado.web.authenticated
     @render
     def get(self):
-        return st('user/login.html')
+        return st('user/login.html',message='登陆')
+
+    def post(self):
+         name = self._get_argument('login_name')
+         passwd = self.get_argument('password')
+
+         if not check(name,passwd):
+             return self.write("账户名或者密码错误！")
+
+         self.set_secure_cookie('usr',name)
+         self.redirect('/home')
+
+
+
+
+
