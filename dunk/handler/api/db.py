67a3477ecdf983fa64bed23e44f59b29a787e453
niaochaobao <niaochaobao@163.com>
2014-12-22 11:43:57 +0800

    目录结构调整

--- dunk/handler/api/db.py
+++
@@ -1,7 +0,0 @@
-#coding=utf-8
-import torndb
-from tornado.options import define,options,parse_command_line 
-
-define('port',default=8888,help='run on the port',type=int)  
- 
-database=torndb.Connection('localhost','tornadoDB',user='bird',password='db123456')
---
+++ dunk/model/db.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+import torndb
+from tornado.options import define,options,parse_command_line 
+
+define('port',default=8888,help='run on the port',type=int)  
+ 
+database=torndb.Connection('localhost','tornadoDB',user='bird',password='db123456')
