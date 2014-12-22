cc8344c966474af5a402d44632ca249b751f86ca
wangxiaomo <wxm4ever@gmail.com>
2014-12-22 20:41:44 +0800

    rename Register to register

--- dunk/handler/user/register.py
+++ dunk/handler/user/register.py
@@ -1,12 +1,11 @@
-import tornado
 from tornado.web import RequestHandler
 from dunk.utils.template import st
 from dunk.utils.decorators import render
 
-class RegisterHander(RequestHandler):
-    @tornado.web.authenticated
-    @render
 
+class RegisterHander(RequestHandler):
+
+    @render
     def get(self):
         return st("user/register.html")
 
@@ -14,7 +13,7 @@ class RegisterHander(RequestHandler):
         name = self._get_argument('login_name')
         passwd = self.get_argument('password')
         re_passwd = self.get_argument('re_password')
-'''
+
         if passwd != re_passwd:
             self.write("set password wrong ")
         else:
@@ -28,5 +27,4 @@ class RegisterHander(RequestHandler):
                 database.close()
 
          self.redirect('/home')
-'''
 
