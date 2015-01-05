fe01c874221f2c6bbcf1a5b7d304f3991d7feb8e
niaochaobao <niaochaobao@163.com>
2015-01-05 15:54:21 +0800

    revise

--- app.py
+++ app.py
@@ -8,19 +8,13 @@ def make_app():
     
     settings = {
         'debug': True,
-        #'static_path': os.path.join( os.path.dirname(__file__), "static" ),
-        #'template_path': os.path.join( os.path.dirname(__file__), "/dunk/templates" ),
         'cookie_secret': 'bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=',
         'login_url': '/user/login'
-        #'log_function':r'/var/log/httpd.log'
     }
     URL_MAP = ROUTE_MAP + [
          (r'/statics/(.*)', StaticFileHandler, {"path": "./statics"})
     ]
-
     return Application(URL_MAP, **settings)
-
-
 
 if __name__ == '__main__':
     app = make_app()
