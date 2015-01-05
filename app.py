9d8969782398cc13a83781697b149f15ccfe6b69
niaochaobao <niaochaobao@163.com>
2015-01-05 15:51:07 +0800

    revise

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 # coding: utf8
 import os
-from tornado.web import Application
+from tornado.web import Application,StaticFileHandler
 from tornado.ioloop import IOLoop
-
 from dunk.route import ROUTE_MAP
-
 
 def make_app():
     
     settings = {
-        'debug' : True,
-        'static_path' : os.path.join(os.path.dirname(__file__), "static") ,
-        'template_path' : os.path.join(os.path.dirname(__file__), "/dunk/templates") ,
+        'debug': True,
+        #'static_path': os.path.join( os.path.dirname(__file__), "static" ),
+        #'template_path': os.path.join( os.path.dirname(__file__), "/dunk/templates" ),
         'cookie_secret': 'bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=',
-        'login_url':'/user/login',
+        'login_url': '/user/login'
         #'log_function':r'/var/log/httpd.log'
     }
-    return Application(ROUTE_MAP,**settings)
+    URL_MAP = ROUTE_MAP + [
+         (r'/statics/(.*)', StaticFileHandler, {"path": "./statics"})
+    ]
+
+    return Application(URL_MAP, **settings)
+
 
 
 if __name__ == '__main__':
---
+++ dunk/static/1.js
@@ -0,0 +1,8 @@
+/**
+ * Created with PyCharm.
+ * User: BIRDS
+ * Date: 15-1-5
+ * Time: 下午3:36
+ * To change this template use File | Settings | File Templates.
+ */
+alert("static js");
