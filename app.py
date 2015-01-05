4b8ca85e7f8df1b99b29a097e00e26203698bcff
wangxiaomo <wxm4ever@gmail.com>
2015-01-05 15:08:36 +0800

    add static path

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # coding: utf8
 import os
-from tornado.web import Application
+from tornado.web import Application, StaticFileHandler
 from tornado.ioloop import IOLoop
 
 from dunk.route import ROUTE_MAP
@@ -10,13 +10,13 @@ def make_app():
     
     settings = {
         'debug' : True,
-        'static_path' : os.path.join(os.path.dirname(__file__), "static") ,
-        'template_path' : os.path.join(os.path.dirname(__file__), "/dunk/templates") ,
         'cookie_secret': 'bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=',
         'login_url':'/user/login',
-        'log_function':r'/var/log/httpd.log'
     }
-    return Application(ROUTE_MAP)
+    URL_MAP = ROUTE_MAP + [
+        (r'/statics/(.*)', StaticFileHandler, {"path": "./statics"})
+    ]
+    return Application(URL_MAP, **settings)
 
 
 if __name__ == '__main__':
